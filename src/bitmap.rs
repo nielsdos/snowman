deec44a5d5655eba80d63237f3b78f013a323042
nielsdos <7771979+nielsdos@users.noreply.github.com>
2022-07-14 23:26:55 +0200

    More bitmap painting

--- src/bitmap.rs
+++ src/bitmap.rs
@@ -1,24 +1,23 @@
-#[derive(Copy, Clone)]
-pub struct Pixel(pub u8, pub u8, pub u8);
+#[derive(Debug, Copy, Clone)]
+pub struct Color(pub u8, pub u8, pub u8);
+
+impl Color {
+    pub fn from(color: u32) -> Self {
+        Self(color as u8, (color >> 8) as u8, (color >> 16) as u8)
+    }
+}
 
 pub struct Bitmap {
     // TODO: should probably not use a vec?
-    pixels: Vec<Pixel>,
+    pixels: Vec<Color>,
     width: u16,
     height: u16,
 }
 
 impl Bitmap {
     pub fn new(width: u16, height: u16) -> Self {
-        let mut pixels = vec![Pixel(255, 0, 0); (width as usize) * (height as usize)];
-        // TODO: this is a test
-        for y in 0..height {
-            for x in 0..width {
-                pixels[(y as usize) * (width as usize) + (x as usize)] = Pixel(y as u8, 0, x as u8);
-            }
-        }
         Self {
-            pixels,
+            pixels: vec![Color(255, 255, 255); (width as usize) * (height as usize)],
             width,
             height,
         }
@@ -34,10 +33,26 @@ impl Bitmap {
         self.height
     }
 
+    fn index_for(&self, x: u16, y: u16) -> usize {
+        (y as usize) * (self.width as usize) + (x as usize)
+    }
+
     #[inline]
-    pub fn pixel_at_no_checks(&self, x: u16, y: u16) -> Pixel {
-        // TODO: the casting is ugly
-        let index = (y as usize) * (self.width as usize) + (x as usize);
-        self.pixels[index]
+    pub fn pixel_at_no_checks(&self, x: u16, y: u16) -> Color {
+        self.pixels[self.index_for(x, y)]
+    }
+
+    pub fn draw_horizontal_line(&mut self, x_start: u16, y: u16, x_to: u16, color: Color) {
+        // TODO: bounds checks to saturate the bounds?
+        let start_index = self.index_for(x_start, y);
+        let end_index = self.index_for(x_to, y);
+        self.pixels[start_index..end_index].fill(color);
+    }
+
+    pub fn fill_rectangle(&mut self, x_from: u16, y_from: u16, x_to: u16, y_to: u16, color: Color) {
+        // TODO: bounds checks to saturate the bounds?
+        for y in y_from..y_to {
+            self.draw_horizontal_line(x_from, y, x_to, color);
+        }
     }
 }
