735b94701b1bf2ebba18cc6e44708af05edaf50b
Niels Dossche <niels.dossche@ugent.be>
2022-07-21 13:58:57 +0200

    Reorganise the bitmap structure a bit and add a way to draw arbitrary lines

--- src/bitmap.rs
+++ src/bitmap.rs
@@ -1,5 +1,6 @@
 use crate::two_d::{Point, Rect};
 use std::ops::{Deref, DerefMut};
+use crate::object_environment::Pen;
 
 #[derive(Debug, Copy, Clone)]
 pub struct Color(pub u8, pub u8, pub u8);
@@ -26,6 +27,7 @@ pub struct Bitmap {
     // TODO: should probably not use a vec?
     pixels: Vec<Color>,
     translation: Point,
+    moved_to: Point,
     width: i16,
     height: i16,
 }
@@ -62,30 +64,38 @@ impl<'a> Drop for BitmapView<'a> {
 
 impl Bitmap {
     pub fn new(width: i16, height: i16) -> Self {
+        // TODO: remove this size hack
+        let width = 500;
+        let height = 500;
         Self {
-            pixels: vec![Color(255, 255, 255); (width as usize) * (height as usize)],
+            pixels: vec![Color(100, 0, 100); (width as usize) * (height as usize)],
             translation: Point::origin(),
+            moved_to: Point::origin(),
             width,
             height,
         }
     }
 
-    fn clip_x(&self, x: i16) -> i16 {
-        // TODO: edge cases?
-        x.clamp(0, self.width.saturating_sub(self.translation.x))
+    pub fn move_to(&mut self, point: Point) {
+        self.moved_to = point;
     }
 
-    fn clip_y(&self, y: i16) -> i16 {
-        // TODO: edge cases?
-        y.clamp(0, self.height.saturating_sub(self.translation.y))
+    fn clip_and_translate_x(&self, x: i16) -> i16 {
+        let x = x.wrapping_add(self.translation.x);
+        x.clamp(0, self.width - 1)
     }
 
-    pub fn clip(&self, rect: Rect) -> Rect {
+    fn clip_and_translate_y(&self, y: i16) -> i16 {
+        let y = y.wrapping_add(self.translation.y);
+        y.clamp(0, self.height - 1)
+    }
+
+    pub fn clip_and_translate_rect(&self, rect: Rect) -> Rect {
         Rect {
-            left: self.clip_x(rect.left),
-            top: self.clip_y(rect.top),
-            right: self.clip_x(rect.right),
-            bottom: self.clip_y(rect.bottom),
+            left: self.clip_and_translate_x(rect.left),
+            top: self.clip_and_translate_y(rect.top),
+            right: self.clip_and_translate_x(rect.right),
+            bottom: self.clip_and_translate_y(rect.bottom),
         }
     }
 
@@ -100,8 +110,6 @@ impl Bitmap {
     }
 
     fn index_for(&self, x: i16, y: i16) -> usize {
-        let x = x.wrapping_add(self.translation.x);
-        let y = y.wrapping_add(self.translation.y);
         (y as usize) * (self.width as usize) + (x as usize)
     }
 
@@ -110,13 +118,20 @@ impl Bitmap {
         self.pixels[self.index_for(x, y)]
     }
 
-    fn draw_horizontal_line_unclipped(&mut self, x_start: i16, y: i16, x_to: i16, color: Color) {
+    pub fn set_pixel(&mut self, x: i16, y: i16, color: Color) {
+        let x = self.clip_and_translate_x(x);
+        let y = self.clip_and_translate_y(y);
+        let index = self.index_for(x, y);
+        self.pixels[index] = color;
+    }
+
+    fn draw_horizontal_line_unclipped_untranslated(&mut self, x_start: i16, y: i16, x_to: i16, color: Color) {
         let start_index = self.index_for(x_start, y);
         let end_index = self.index_for(x_to, y);
         self.pixels[start_index..end_index].fill(color);
     }
 
-    fn draw_vertical_line_unclipped(&mut self, x: i16, y_start: i16, y_to: i16, color: Color) {
+    fn draw_vertical_line_unclipped_untranslated(&mut self, x: i16, y_start: i16, y_to: i16, color: Color) {
         for y in y_start..y_to {
             let index = self.index_for(x, y);
             self.pixels[index] = color;
@@ -124,28 +139,60 @@ impl Bitmap {
     }
 
     pub fn draw_vertical_line(&mut self, x: i16, y_start: i16, y_to: i16, color: Color) {
-        self.draw_vertical_line_unclipped(
-            self.clip_x(x),
-            self.clip_y(y_start),
-            self.clip_y(y_to),
+        self.draw_vertical_line_unclipped_untranslated(
+            self.clip_and_translate_x(x),
+            self.clip_and_translate_y(y_start),
+            self.clip_and_translate_y(y_to),
             color,
         )
     }
 
     pub fn draw_horizontal_line(&mut self, x_start: i16, y: i16, x_to: i16, color: Color) {
-        self.draw_horizontal_line_unclipped(
-            self.clip_x(x_start),
-            self.clip_y(y),
-            self.clip_x(x_to),
+        self.draw_horizontal_line_unclipped_untranslated(
+            self.clip_and_translate_x(x_start),
+            self.clip_and_translate_y(y),
+            self.clip_and_translate_x(x_to),
             color,
         )
     }
 
     pub fn fill_rectangle(&mut self, rect: Rect, color: Color) {
-        let rect = self.clip(rect);
-        // TODO: check validity of rect
-        for y in rect.top..rect.bottom {
-            self.draw_horizontal_line_unclipped(rect.left, y, rect.right, color);
+        let rect = self.clip_and_translate_rect(rect);
+        if rect.left < rect.right && rect.top < rect.bottom {
+            for y in rect.top..rect.bottom {
+                self.draw_horizontal_line_unclipped_untranslated(rect.left, y, rect.right, color);
+            }
         }
+    }
+
+    fn draw_line(&mut self, from: Point, to: Point, pen: &Pen) {
+        // TODO: different pen styles
+        let dx = to.x.wrapping_sub(from.x).abs();
+        let dy = -to.y.wrapping_sub(from.y).abs();
+        let mut err = dx.wrapping_add(dy);
+        let sx = if from.x < to.x { 1 } else { -1 };
+        let sy = if from.y < to.y { 1 } else { -1 };
+        let mut current_x = from.x;
+        let mut current_y = from.y;
+
+        loop {
+            self.set_pixel(current_x, current_y, pen.color);
+            if current_x == to.x && current_y == to.y {
+                break;
+            }
+            let err2 = err.wrapping_mul(2);
+            if err2 >= dy {
+                err = err.wrapping_add(dy);
+                current_x = current_x.wrapping_add(sx);
+            }
+            if err2 <= dx {
+                err = err.wrapping_add(dx);
+                current_y = current_y.wrapping_add(sy);
+            }
+        }
+    }
+
+    pub fn line_to(&mut self, to: Point, pen: &Pen) {
+        self.draw_line(self.moved_to, to, pen)
     }
 }
